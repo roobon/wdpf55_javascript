7cda6363d96ad7df42e3ad0f863a32da058d90f2
mizanur1326 <Iamuser@94>
2023-06-07 19:51:51 +0600

    js practise self

---
+++ Mizanur_1277622/Stack_Learner/Loop/while.js
@@ -0,0 +1,13 @@
+// let i = 1;
+// while(i <=100){
+//     console.log(i)
+//     i++;
+// }
+
+let sum = 0; //while loop used for unknown start and ending value.
+let i = 1;
+while (i <=5){
+    sum += i;   
+    i++;
+}
+console.log(sum);

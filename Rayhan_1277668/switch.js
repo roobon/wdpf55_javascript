a43fd816492ebeddad49ee51f67ca107067768b7
rockRayhan <khayrulalamdict@gmail.com>
2023-06-12 13:09:10 +0600

    asd

---
+++ Rayhan_1277668/switch.js
@@ -0,0 +1,15 @@
+var color = 'yellow' ; 
+
+switch (color){
+    case 'blue':
+        console.log('color is blue');
+        break;
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'white':
+        console.log('color is white');
+        break;
+    default:
+        console.log('color is black'); 
+}

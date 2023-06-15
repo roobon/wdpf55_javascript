28ae6b501f778e17fe65c5030074cca81a0a2a56
Alauddin007-hub <mdalauddinalsafin@gmail.com>
2023-06-15 13:21:53 +0600

    classwork 15/05/2023

---
+++ Md_Alauddin_1273850/oddeven.js
@@ -0,0 +1,9 @@
+
+        function oddeven(a){
+            if( a % 2 == 0){
+                return a + " is a Even number"
+            } else {
+                return a + " is a Odd number"
+            }
+        }
+        console.log(oddeven(7))

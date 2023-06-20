c5e849a7a406495cc7bc50cfdcb68db98947fe1a
rockRayhan <khayrulalamdict@gmail.com>
2023-06-20 13:11:39 +0600

    hello

---
+++ Rayhan_1277668/turnery_operator.js
@@ -0,0 +1,16 @@
+//  ========= If Else  ==========
+
+//  let x = 10 ;
+
+//  if ( x > 0 ) {
+//     console.log("good");
+//  } else {
+//     console.log(" bad ");
+//  }
+
+
+//  ========= Turnery Operator ==========
+let x = 10;
+
+let result = (x > 0) ? "GOOD"  : "Bad";
+console.log(result);

12d243d2f00c2b59ed5a0bc55d655709e0d16cac
unknown <kamrulislam.serzy50@gmail.com>
2023-07-20 09:39:19 +0600

    array

--- Mamun-1277681/javascript/Array/AssociateArray.php
+++ Mamun-1277681/javascript/Array/AssociateArray.php
@@ -5,6 +5,7 @@ $assocArray = [
     'age' => 30,
     'email' => 'john@example.com'
 ];
+echo"<pre>";
 print_r($assocArray)
 
 ?>
--- Mamun-1277681/javascript/Array/multidiemntionalArray.php
+++ Mamun-1277681/javascript/Array/multidiemntionalArray.php
@@ -5,5 +5,6 @@ $multiArray = [
     array(4, 5, 6),
     array(7, 8, 9)
 ];
+echo"<pre>";
 print_r($multiArray)
 ?>

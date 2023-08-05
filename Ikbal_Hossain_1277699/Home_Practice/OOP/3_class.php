17204f9870648ecad312109f93b131f6d61b0404
Ikbal Hossain <ikbalhossain125@gmail.com>
2023-08-05 22:40:07 +0600

    updated

---
+++ Ikbal_Hossain_1277699/Home_Practice/OOP/3_class.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        class Computer {
+            constructor(name, model){
+                this.name = name;
+                this.model = model;
+            }
+        }
+
+        const dell = new Computer("Dell", "dk125");
+        const hp = new Computer("hp", "dd33");
+
+        
+    </script>
+</body>
+</html>

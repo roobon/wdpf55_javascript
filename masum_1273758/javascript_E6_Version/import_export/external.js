d3b3d8327ea4160cd037ba1fd52aae1ef057f06c
masum-yasin <masum55549@gmail.com>
2023-10-14 18:11:45 +0600

    update

---
+++ masum_1273758/javascript_E6_Version/import_export/external.js
@@ -0,0 +1,9 @@
+export const string = "Bangladesh is a beautiful Country";
+const string2 = "Bangladeshi is most population Country";
+ export default string2;
+
+
+//  ///////////
+ function myfunc (){
+    console.log("I love Technology of my wish");
+ }
---
+++ masum_1273758/javascript_E6_Version/import_export/playground.js
@@ -0,0 +1,2 @@
+import    external ,{string, myfunc} from "./external.js"
+console.log(external,string);
